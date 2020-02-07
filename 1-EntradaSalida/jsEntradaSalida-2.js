0a1887dc3fd370d5285ef3631a371783eb1777bc
frak-star <60760338+frak-star@users.noreply.github.com>
2020-02-06 22:15:04 -0300

    Add files via upload
    clase 1

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,16 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
-{
-	
+{var nombre;
+    nombre="juan",
+
+    prompt("Ingrese su nombre");
+
+    alert(nombre);
+    
+
+
+ 
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,13 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
+    var nombre;
+    nombre = document.getElementById("elNombre").value;
+
+    var edad;
+    edad = documente.getElementById("laEdad").value;
+
+    alert(nombre);
+    
 }
 
